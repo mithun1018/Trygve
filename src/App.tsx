58bdb2730d854fb0189fde4692124775484947a4
mithun1018 <kingmithunking38@gmail.com>
2025-07-15 13:06:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Trygve</h1>
+      <p>Your journey starts here!</p>
+      {/* Other components and content can be added here */}
+    </div>
+    
+  )
+}
+
+export default App
